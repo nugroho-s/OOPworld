0b9cd5b0d4c0bb51d20fb391b14e7445e50842e0
albertusk95 <albertusk15@gmail.com>
2016-03-12 01:28:46 +0700

    updated board2

--- OOPworld/OOPworld/board2.h
+++ OOPworld/OOPworld/board2.h
@@ -3,7 +3,6 @@
 
 extern int sizex;
 extern int sizey;
-extern int sizepintu_p;
 extern Point min_h_vertical, min_k_vertical;
 extern Point max_h_vertical, max_k_vertical;
 extern Point min_h_horizontal, min_k_horizontal;
@@ -11,23 +10,22 @@ extern Point max_h_horizontal, max_k_horizontal;
 extern char** isi;
 extern Point pintu_u;
 extern Point pintu_s;
-extern Point* pintu_p;
-
+		
 void board();
-// make board dari file eksternal 
+	// make board dari file eksternal 
 void min_h_ver(char, int*, int*);
 void max_h_ver(char, int*, int*);
 void min_h_hor(char, int*, int*);
 void max_h_hor(char, int*, int*);
 void printboard();
-//print kondisi dunia saat ini
+	//print kondisi dunia saat ini
 void tambah(makhluk&, char);
-//tambahkan makhluk ke papan
+	//tambahkan makhluk ke papan
 void move(makhluk&, char);
-//gerakkan makhluk, parameter makhluk yang diminta parameter
-//adalah sebelum digerakkan
-//setelah move, makhluk akan berpindah
+	//gerakkan makhluk, parameter makhluk yang diminta parameter
+	//adalah sebelum digerakkan
+	//setelah move, makhluk akan berpindah
 void hapus(makhluk&);
-//hapus makhluk dari papan
+	//hapus makhluk dari papan
 void deallocate_board();
-//dealokasi array board
+	//dealokasi array board
